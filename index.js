aa39b00a56da7c2688eb2d4c8101e9cf152c0914
christin-weston <christin.weston@outlook.com>
2026-06-10 14:20:40 -0700

    Update response message for test deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
-  res.end('Hello Helsinki!')
+  res.end('Hello Helsinki Test Deployment!')
 })
 const port = process.env.PORT || 3000
 server.listen(port, () => console.log(`Running on port ${port}`))
